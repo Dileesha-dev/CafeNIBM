71bb02c90d414db0734288f7ae01944dcb325630
Dileesha Nayanajith <dileeshanayanajith123@gmail.com>
2021-03-03 15:11:32 +0530

    initial commit

---
+++ DileeshaNayanajith-COBSCComp192-056/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DileeshaNayanajith-COBSCComp192-056
+//
+//  Created by user162257 on 3/3/21.
+//  Copyright © 2021 user162257. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
