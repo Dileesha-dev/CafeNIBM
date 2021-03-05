070fcd658b98142191a05c69d54bd9d35a330659
Dileesha Nayanajith <dileeshanayanajith123@gmail.com>
2021-03-05 22:37:00 +0530

    added rounded corners to login button

--- DileeshaNayanajith-COBSCComp192-056/ViewController.swift
+++ DileeshaNayanajith-COBSCComp192-056/ViewController.swift
@@ -9,10 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var signInButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        signInButton.layer.cornerRadius = 25
     }
 
 
