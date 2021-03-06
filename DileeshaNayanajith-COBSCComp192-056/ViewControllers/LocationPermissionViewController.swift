bf30fdc88bf64cd692fe2eedd273f15ee95f0ec8
Dileesha Nayanajith <dileeshanayanajith123@gmail.com>
2021-03-06 23:31:35 +0530

    added location permission view and navigation

---
+++ DileeshaNayanajith-COBSCComp192-056/ViewControllers/LocationPermissionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LocationPermissionViewController.swift
+//  DileeshaNayanajith-COBSCComp192-056
+//
+//  Created by user162257 on 3/6/21.
+//  Copyright © 2021 user162257. All rights reserved.
+//
+
+import UIKit
+
+class LocationPermissionViewController: UIViewController {
+    @IBOutlet weak var permissionGrantedButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        permissionGrantedButton.layer.cornerRadius = 18    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
